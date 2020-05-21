1e8a0f457e5b2a5562ef3c33946e32446f3dbc67
Sebastian Otarola <otarolasebas@hotmail.com>
2020-05-21 13:52:53 -0600

    Primer commit con cambios del simulador se pueden meter cantidad de carros, tiempo y demás. Además se fijan variables de velocidad

---
+++ js/prueba.js
@@ -0,0 +1,4 @@
+
+arregloCircunvalacion =[2553,2394,2443,2179];
+arregloEntrada = [1111,1051,1072,956];
+setParametros(arregloEntrada[0],arregloCircunvalacion[0],10000);
---
+++ js/pruebaCsv.js
@@ -0,0 +1,2 @@
+var Flujo = detectors[1].historyFlow[detectors[1].iAggr];
+console.log("Este es el flujo "+ Flujo);
